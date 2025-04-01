4933d8fd96b6746607f4a3d29af34e019b06cdb9
Abhilash2219 <abhilashbadiger0000@gmail.com>
2025-04-01 22:04:38 +0530

    updated code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css';
-import type { AppProps } from "next/app";
+import '../styles/globals.css'; // Global styles
+import '../styles/certificates.css'; // Add any other global styles here
+import '../styles/experience.css';  // Add any other global styles here
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
