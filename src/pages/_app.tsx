9dd215ba40a1940c38d52125a02a875235f94bf9
Abhilash <abhilashbadiger0000@gmail.com>
2025-04-19 14:02:37 +0530

    css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,5 @@
 import '../styles/globals.css'; // Global styles
-import "../styles/certificates.css";
-import "../styles/experience.css";
-import "../styles/footer.css";
-import "../styles/header.css";
-import "../styles/hero.css";
-import "../styles/modal.css";
-import "../styles/projects.css";
-import "../styles/skills.css";
-import '../styles/socialbar.css';
+
  // Add any other global styles here
 import type { AppProps } from 'next/app';
 
